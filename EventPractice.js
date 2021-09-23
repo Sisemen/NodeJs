e5bad6658188d2f53e7cc0b5f12946e68e5c434f
Ali Khaleghifard <ali.khaleghifard@tuni.fi>
2021-09-23 14:56:52 +0300

    Emission of an event has been tested successfully.

--- EventPractice.js
+++ EventPractice.js
@@ -1,1 +1,29 @@
+var events = require('events');
 
+class Person{
+  constructor(name){
+    this.personName = name;
+  }
+
+  personName = '';
+
+  introduceYourself = function(name){
+    console.log(`My name is ${name}.`);
+  };
+
+  eventEmitter = new events.EventEmitter();
+}
+
+var ali = new Person('Ali');
+var gholi = new Person('Gholi');
+var hamurabi = new Person('Hamurabi');
+
+var people = [ali, gholi, hamurabi];
+
+people.forEach(person => {
+  person.eventEmitter.on('introduceYourself', person.introduceYourself);
+});
+
+people.forEach(person => {
+  person.eventEmitter.emit('introduceYourself', person.personName);
+});
