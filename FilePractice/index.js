2c06cdd2e7064f94c534600355795aeada0c3b7d
Ali Khaleghifard <ali.khaleghifard@tuni.fi>
2021-09-28 03:52:34 +0300

    Make it a little bit sexy.

--- FilePractice/index.js
+++ FilePractice/index.js
@@ -1,12 +1,50 @@
 var fs = require('fs');
 var filename = __dirname + '/index.js';
+var duplicate_file_storage = __dirname + '/DuplicateFileStorage';
+var index_duplicate = duplicate_file_storage + '/index_duplicate.js';
+var gitignore = __dirname + '/.gitignore';
 
 fs.readFile(filename, 'utf8', (error, data) => {
-  console.log(data);
-
-  fs.writeFile(filename, data + '\r\n\r\nconsole.log(\'Added code to the end of file.\');', (error) => {
-    console.info(fs.readFileSync(filename, 'utf8'));
+  fs.access(index_duplicate, (err) => {
+    if (err === null) {
+      fs.unlink(index_duplicate, (err) => {
+        if(err !== null)
+          console.error(err);
+        else
+          fs.rmdir(duplicate_file_storage, (err) => {
+            if (err !== null) {
+              console.error(err);
+            }
+            else {
+              create_file(data);
+            }
+          });
+      });
+    }
+    else if (err.code !== 'ENOENT') {
+      console.error(err);
+    }
+    else {
+      create_file(data);
+    }
   });
 });
 
-console.log('Added code to the end of file.');
+function create_file(content) {
+  fs.mkdir(duplicate_file_storage, (err) => {
+    if(err === null) {
+      var file_content = '// This file is just a duplicate of the \"index.js\".\r\n\r\n' + content;
+
+      fs.writeFile(index_duplicate, file_content, (err) => {
+        if(err !== null) {
+          console.error(err);
+        }
+      });
+
+      fs.access(gitignore, (err) => {
+        if (err && err.code === 'ENOENT')
+          fs.writeFile(__dirname + '/.gitignore', 'DuplicateFileStorage/', () => { });
+      });
+    }
+  });
+}
