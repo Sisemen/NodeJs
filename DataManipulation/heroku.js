df912f504584fe301cde0ab2f955f819568ed29c
Ali Khaleghifard <ali.khaleghifard@tuni.fi>
2021-10-13 03:45:08 +0300

    Using config.json file.

--- DataManipulation/heroku.js
+++ DataManipulation/heroku.js
@@ -1,7 +1,8 @@
 const { Client } = require('pg');
+const config = require('./config.json');
 
 const client = new Client({
-  connectionString: 'postgres://bgfccnftujnchm:f3e55a595741045397b9d3fd38636cf085fbf170cd31c02fb44d9406360e8593@ec2-54-154-101-45.eu-west-1.compute.amazonaws.com:5432/dbhqqep4d1t5r8',
+  connectionString: config.connectionString,
   ssl: {
     rejectUnauthorized: false
   }
